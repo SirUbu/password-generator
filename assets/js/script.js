2c3edbd9593d226edbc9e90efa2436b5b06e74c4
Sir Ubu <thesirubu@gmail.com>
2021-03-31 15:13:03 -0600

    Added function for password lenght.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 // Assignment code here
+var generatePassword = function() {
+  var characterLength= passwordLength(); 
 
+  return characterLength;
+};
+
+// function to get password length
+var passwordLength = function() {
+  // prompt user for length
+  var length = parseInt(prompt("How long would you like your password to be? \nMinimum length is 8, max length is 128. \nPlease enter a number from 8 to 128:"));
+
+  // ensure user entry is acceptable response
+  while (length < 8 || length > 128 || !Number.isInteger(length)) {
+    length = parseInt(prompt("Please enter a valid number from 8 to 128."));
+  }
+  return length;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -11,7 +27,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
